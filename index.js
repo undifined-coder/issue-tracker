65ca54fd780c5e165a39e98a0e8964fbdef56217
undifined-coder <130596564+undifined-coder@users.noreply.github.com>
2023-07-20 23:46:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-require('dotenv').config();  //Load Env by dotenv
 const express = require('express');
 
-const db = require('./config/mongoose');
-const port = process.env.PORT || 3000;
-const app = express();
-const path = require('path');
-const expressLayouts = require('express-ejs-layouts');//express ejs-layout
+const router = express.Router();
+const homeController = require('../controller/home_controller');
 
-app.use(express.urlencoded());
-app.use(express.static('assets'));
-app.use(expressLayouts);
+console.log('router loaded');
 
-// extract style and scripts
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+router.get('/', homeController.home);
+router.use('/project', require('./project'));
 
-// set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-// use express router
-app.use('/', require('./routes'));
-app.listen(port, function (err) {
-  if (err) {
-    console.log(`Error in running the server: ${err}`);
-  }
-  console.log(`Server is running on port: ${port}`);
-});
+module.exports = router;
